afe2c6d6f6cf965b7dfb51c0ddf314b69a9d8591
Ulises Rendón <fdulises@outlook.com>
2021-06-11 18:42:40 -0500

    Ordenando y arreglos pequeños

--- liweditor/editor-main.js
+++ liweditor/editor-main.js
@@ -1,3 +1,3 @@
-var myeditor = new liweditor("#myeditor");
+const myeditor = new liweditor("#myeditor");
 //Se requiere llamar el siguiente metodo si se desea obtener el textarea.value
-myeditor.update();
+//myeditor.update();
