29993a62818c320377c6517a5f9b6e3d536c7075
Ulises Rendón <fdulises@outlook.com>
2021-07-03 02:39:00 -0500

    Probando con otro html formater

--- theme/js/lieditor.js
+++ theme/js/lieditor.js
@@ -26,7 +26,7 @@ class lieditor {
     //Definimos funcionamiento del metodo update
     value() {
         if (this.textarea.getAttribute("data-state") == "hidden")
-            this.textarea.value = lieditor.format(this.editable.innerHTML);
+            this.textarea.value = this.editable.innerHTML;
         return this.textarea.value;
     };
 
@@ -99,23 +99,75 @@ class lieditor {
         return (match && match[7].length == 11) ? match[7] : false;
     }
     static format(html) {
-        var tab = '\t';
-        var result = '';
-        var indent = '';
+        let tab = '\t', result = '', indent = '';
 
         html.split(/>\s*</).forEach(element => {
-            if (element.match(/^\/\w/)) {
-                indent = indent.substring(tab.length);
-            }
-
+            if (element.match(/^\/\w/)) indent = indent.substring(tab.length);
             result += indent + '<' + element + '>\r\n';
-
             if (element.match(/^<?\w[^>]*[^\/]$/) && !element.startsWith("input")) {
                 indent += tab;
             }
         });
 
         return result.substring(1, result.length - 3);
+    }
+    static formatHTML(html) {
+        let indent = '\n', tab = '\t', i = 0, pre = [];
+
+        html = html
+            .replace(new RegExp('<pre>((.|\\t|\\n|\\r)+)?</pre>'), function (x) {
+                pre.push({ indent: '', tag: x });
+                return '<--TEMPPRE' + i++ + '/-->'
+            })
+            .replace(new RegExp('<[^<>]+>[^<]?', 'g'), function (x) {
+                let ret;
+                let tag = /<\/?([^\s/>]+)/.exec(x)[1];
+                let p = new RegExp('<--TEMPPRE(\\d+)/-->').exec(x);
+
+                if (p) pre[p[1]].indent = indent;
+
+                if (['area', 'base', 'br', 'col', 'command', 'embed', 'hr', 'img', 'input', 'keygen', 'link', 'menuitem', 'meta', 'param', 'source', 'track', 'wbr'].indexOf(tag) >= 0) // self closing tag
+                    ret = indent + x;
+                else {
+                    if (x.indexOf('</') < 0) { //open tag
+                        if (x.charAt(x.length - 1) !== '>')
+                            ret = indent + x.substr(0, x.length - 1) + indent + tab + x.substr(x.length - 1, x.length);
+                        else
+                            ret = indent + x;
+                        !p && (indent += tab);
+                    }
+                    else {//close tag
+                        indent = indent.substr(0, indent.length - 1);
+                        if (x.charAt(x.length - 1) !== '>')
+                            ret = indent + x.substr(0, x.length - 1) + indent + x.substr(x.length - 1, x.length);
+                        else
+                            ret = indent + x;
+                    }
+                }
+                return ret;
+            });
+
+        for (i = pre.length; i--;) {
+            html = html.replace('<--TEMPPRE' + i + '/-->', pre[i].tag.replace('<pre>', '<pre>\n').replace('</pre>', pre[i].indent + '</pre>'));
+        }
+
+        return html.charAt(0) === '\n' ? html.substr(1, html.length - 1) : html;
+    }
+    static unformatHTML(html) {
+        var i = 0;
+        var pre = [];
+
+        html = html.replace(new RegExp('<pre>((.|\\t|\\n|\\r)+)?</pre>'), function (x) {
+            pre.push(x);
+            return '<--TEMPPRE' + i++ + '/-->'
+        }).replace(/\n/g, '').replace(/\t/g, '');
+
+        for (i = pre.length; i--;) {
+            html = html.replace('<--TEMPPRE' + i + '/-->', pre[i]);
+        }
+
+        html = html.replace(new RegExp('<pre>\\n'), '<pre>').replace(new RegExp('\\n\\t*</pre>'), '</pre>');
+        return html;
     }
 }
 
@@ -273,7 +325,7 @@ let lieditorinput = {
         class: 'btn', type: 'button', event: 'click',
         action: e=>{
             if (e.editor.textarea.getAttribute("data-state") == "hidden") {
-                e.editor.textarea.value = lieditor.format(e.editor.editable.innerHTML);
+                e.editor.textarea.value = lieditor.formatHTML(lieditor.unformatHTML(e.editor.editable.innerHTML));
                 e.editor.textarea.setAttribute("data-state", "showed");
                 e.editor.editable.setAttribute("data-state", "hidden");
             } else {
